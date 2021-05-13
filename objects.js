3db2e6ec3a95ef7004d5ea159ea0ea731d777e46
Forest Traylor <foresttraylor@gmail.com>
2021-05-12 19:56:29 -0700

    created person class

--- objects.js
+++ objects.js
@@ -1,3 +1,5 @@
+// ze hamster class and ze methods
+
 class Hamster {
   constructor(name){
     this.owner = ''
@@ -12,5 +14,51 @@ class Hamster {
   }
   getPrice() {
     return this.price
+  }
+}
+
+
+// ze person and ze methods
+
+class Person {
+  constructor(name) {
+    this.name = name
+    this.age = 0
+    this.height = 0
+    this.weight = 0
+    this.mood = 0
+    this.hamsters = []
+    this.bankAccount = 0
+  }
+  getName() {
+    return this.name;
+  }
+  getAge() {
+    return this.age;
+  }
+  getWeight() {
+    return this.weight;
+  }
+  greet() {
+    console.log('hello, ' + this.name);
+  }
+  eat() {
+    this.weight++;
+    this.mood++;
+  }
+  exercise() {
+    this.weight--;
+  }
+  ageUp() {
+    this.age++;
+    this.height++;
+    this.weight++;
+    this.mood--;
+    this.bankAccount+=10;
+  }
+  buyHamster(hamster) {
+    this.hamsters.push(hamster);
+    this.mood+=10;
+    this.bankAccount-=hamster.getPrice();
   }
 }
