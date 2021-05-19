cad03c9ed8898108e1e888b9f6a2ab891aa0e1d4
Forest Traylor <foresttraylor@gmail.com>
2021-05-18 19:18:28 -0700

    created pizza rat

--- donut_adventure/adventure.js
+++ donut_adventure/adventure.js
@@ -1,6 +1,4 @@
-//i had to write these out in a replit because github was giving me issues so the push times look almost identical
-
-// hero
+// hero class
 
 class Hero {
   constructor(name) {
@@ -27,6 +25,33 @@ class Hero {
 
 let doug = new Hero("Dougie the Donut");
 
+// enemy class
 
+class Enemy {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.weapons = {
+      pepperoniStars: 5,
+      cheeseGrease: 10
+    };
+    this.catchPhrases = ['i\'m youtube famous',
+        'i\'m more dangerous than an uncovered sewer']
+  }
+  talkSmack() {
+    let quote = this.catchPhrases[Math.floor(Math.random()*2)];
+    console.log(quote);
+  }
+  announceHealth() {
+    console.log(this.name + " HP: " + this.health);
+  }
+  fight() {
+    console.log("i\'m gonna flatten you like a slice of pepperoni!");
+  }
+  fightAgain() {
+    console.log(this.name + "used Cheese Grease for 10HP")
+    console.log(target.health --10);
+  }
+}
 
- 
+let rat = new Enemy("Pizza Rat")
